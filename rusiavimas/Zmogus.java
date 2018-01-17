978e2d6fe5e8587c4319c65ddb1b0cfb2d4ad878
vytautasphp <33592543+vytautasphp@users.noreply.github.com>
2018-01-17 13:17:40 +0200

    rusiavimas maine

---
+++ rusiavimas/Zmogus.java
@@ -0,0 +1,19 @@
+package rusiavimas;
+
+public class Zmogus {
+
+    String vardas;
+
+    String pavarde;
+
+
+
+    Zmogus(String vardas, String pavarde) {
+        this.vardas = vardas;
+        this.pavarde = pavarde;
+    }
+    Zmogus(String vardas) {
+        this.vardas = vardas;
+
+    }
+}
