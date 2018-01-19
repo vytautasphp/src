604c927fc0a704d23a171d3d7c9b2bc3aacb2a50
vytautasphp <33592543+vytautasphp@users.noreply.github.com>
2018-01-19 21:19:07 +0200

    07 pratimas

---
+++ pratimas07/Mokinys.java
@@ -0,0 +1,39 @@
+package pratimas07;
+
+public class Mokinys extends Zmogus {
+
+    public static int[] kl = new int[12];
+    private int klase; // pvz 6 ar 9
+    private int[] trimestras; // masyvas mokinio trimestro pažymių
+
+    public int getKlase() {
+        return klase;
+    }
+
+    private int[] getTrimestras() {
+        return trimestras;
+    }
+
+    Mokinys(String vardas, String pavarde, int klase, int[] trimestras) {
+        super(vardas, pavarde);
+        this.klase = klase;
+        this.trimestras = trimestras;
+        int kint = klase;
+        kint -= 1;
+        kl[kint]++;
+    }
+
+    //vidurkio skaiciavimas//
+    public double getVidurkis() {
+        double suma = 0;
+        double vidurkis;
+        double vidurkisr;
+
+        for (int i = 0; i < getTrimestras().length; i++) {
+            suma += getTrimestras()[i];
+        }
+        vidurkisr = suma / getTrimestras().length;
+        vidurkis = Math.round(vidurkisr);
+        return vidurkis;
+    }
+}
