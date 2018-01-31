cb7115f5511da4631437e605b6a0dcd46781c40b
vytautasphp <33592543+vytautasphp@users.noreply.github.com>
2018-01-31 13:01:33 +0200

    13 Uzduotis

--- uzduotis13/Main.java
+++ uzduotis13/Main.java
@@ -1,9 +1,8 @@
 package uzduotis13;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
